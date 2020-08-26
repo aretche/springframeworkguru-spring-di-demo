456a0833161b8ad1a3d25b021d900fb2d9002019
arellanog <arellanog@cent.gov.ar>
2020-08-26 14:53:25 -0300

    Initial commit

---
+++ src/test/java/aretche/springdidemo/SpringDiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package aretche.springdidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
